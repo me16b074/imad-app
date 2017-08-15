84340a5a077800076a2b2eb08a2acdc378054945
me16b074 <30663556+me16b074@users.noreply.github.com>
2017-08-15 17:26:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artcle-one', function(req, res){
+   res.send('Article one served and will be requested here.'); 
+});
+
+app.get('/artcle-two', function(req, res){
+   res.send('Article two served and will be requested here.'); 
+});
+
+app.get('/artcle-three', function(req, res){
+   res.send('Article three served and will be requested here.'); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
